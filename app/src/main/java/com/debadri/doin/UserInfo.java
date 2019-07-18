37cf6cc4438dbdb967c2ad3f2c843185d53ad859
debadri16 <debadri16@gmail.com>
2019-07-18 19:08:15 +0530

    first upload

---
+++ app/src/main/java/com/debadri/doin/UserInfo.java
@@ -0,0 +1,24 @@
+package com.debadri.doin;
+
+import io.realm.RealmObject;
+
+public class UserInfo extends RealmObject {
+    String userName;
+    String others;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getOthers() {
+        return others;
+    }
+
+    public void setOthers(String others) {
+        this.others = others;
+    }
+}
